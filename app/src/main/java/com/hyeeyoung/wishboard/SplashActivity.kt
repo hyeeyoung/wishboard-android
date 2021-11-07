34573e8b0f3fa3b539f5d2f34c603675c4411379
youngjinc <cyjin6789@gmail.com>
2021-11-07 21:30:40 +0900

    [feat] 인트로 구현

--- app/src/main/java/com/hyeeyoung/wishboard/SplashActivity.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/SplashActivity.kt
@@ -1,15 +1,33 @@
 package com.hyeeyoung.wishboard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import com.hyeeyoung.wishboard.databinding.ActivitySplashBinding
+import com.hyeeyoung.wishboard.view.sign.SignActivity
+import kotlinx.coroutines.*
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
+    private var job: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+
+        lifecycleScope.launch(Dispatchers.Main) {
+            job = launch {
+                delay(2000)
+                startActivity(Intent(this@SplashActivity, SignActivity::class.java))
+                finish()
+            }
+        }
+    }
+
+    override fun onPause() {
+        job?.cancel()
+        super.onPause()
     }
 }
