22311f7efd9008a5c84041dea9dc53b8ae8f0287
youngjinc <cyjin6789@gmail.com>
2021-12-26 14:04:40 +0900

    [add]  Glide import 추가

--- app/src/main/java/com/hyeeyoung/wishboard/view/cart/adapters/CartItemListAdapter.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/view/cart/adapters/CartItemListAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.hyeeyoung.wishboard.databinding.ItemCartBinding
 import com.hyeeyoung.wishboard.model.cart.CartItem
 
