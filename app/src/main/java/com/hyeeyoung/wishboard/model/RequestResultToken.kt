abb1be7f163689028eb5716b29b63098fe697cd2
youngjinc <cyjin6789@gmail.com>
2022-02-02 16:57:30 +0900

    [mod] 파일명 변경

--- app/src/main/java/com/hyeeyoung/wishboard/model/RequestResultToken.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/model/RequestResultToken.kt
@@ -1,7 +1,10 @@
 package com.hyeeyoung.wishboard.model
 
+import com.google.gson.annotations.SerializedName
+
 data class RequestResultToken(
     val success: Boolean,
     val message: String,
-    val data: String,
+    @SerializedName("data")
+    val token: String,
 )
