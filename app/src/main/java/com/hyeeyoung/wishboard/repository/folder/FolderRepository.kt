fe9848095a00c291497f0a61615574c0007a6f15
youngjinc <cyjin6789@gmail.com>
2022-02-04 22:59:51 +0900

    [feat] 동일 폴더명 검증 프로세스 구현 및 동일 폴더명 존재할 경우 detail 메세지 보여주기

--- app/src/main/java/com/hyeeyoung/wishboard/repository/folder/FolderRepository.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/repository/folder/FolderRepository.kt
@@ -7,7 +7,17 @@ interface FolderRepository {
     suspend fun fetchFolderList(token: String): List<FolderItem>?
     suspend fun fetchFolderListSummary(token: String): List<FolderItem>?
     suspend fun fetchItemsInFolder(token: String, folderId: Long): List<WishItem>?
-    suspend fun createNewFolder(token: String, folderItemInfo: FolderItem): Long?
-    suspend fun updateFolderName(token: String, folderId: Long, folderName: String): Boolean
+    /** [return type] first.first : isSuccessful, first.second : responseCode, second : folderId */
+    suspend fun createNewFolder(
+        token: String,
+        folderItemInfo: FolderItem
+    ): Pair<Pair<Boolean, Int>, Long?>
+
+    /** [return type] first : isSuccessful, second : responseCode */
+    suspend fun updateFolderName(
+        token: String,
+        folderId: Long,
+        folderName: String
+    ): Pair<Boolean, Int>
     suspend fun deleteFolder(token: String, folderId: Long): Boolean
 }
