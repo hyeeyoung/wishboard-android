af8bb766380b282675f9b8ebda7859bf6db5d9fd
youngjinc <cyjin6789@gmail.com>
2022-02-23 19:56:37 +0900

    [feat] 파이어베이스 동적링크 수신 구현

--- app/src/main/java/com/hyeeyoung/wishboard/MainActivity.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.hyeeyoung.wishboard
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.dynamiclinks.ktx.dynamicLinks
+import com.google.firebase.ktx.Firebase
 import com.hyeeyoung.wishboard.databinding.ActivityMainBinding
 import com.hyeeyoung.wishboard.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,8 +24,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        addDynamicLinkListener()
         viewModel.initFCMToken()
+
         initializeView()
+    }
+
+    private fun addDynamicLinkListener() {
+        Firebase.dynamicLinks
+            .getDynamicLink(intent)
+            .addOnSuccessListener(this) { pendingDynamicLinkData ->
+                var deeplink: Uri? = null
+                if (pendingDynamicLinkData != null) {
+                    deeplink = pendingDynamicLinkData.link
+                }
+
+                if (deeplink != null) {
+                    Log.i(TAG, "deeplink: $deeplink")
+                } else {
+                    Log.d(TAG, "getDynamicLink: no link found")
+                }
+            }
+            .addOnFailureListener(this) { e -> Log.w(TAG, "getDynamicLink:onFailure", e) }
     }
 
     private fun initializeView() {
