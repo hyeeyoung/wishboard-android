0ec8daace6a0dc80070e06b78bbc563a5f8e651c
youngjinc <cyjin6789@gmail.com>
2022-10-04 09:02:56 +0900

    [mod] Timber DebugTree 커스텀

--- app/src/main/java/com/hyeeyoung/wishboard/WishBoardApp.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/WishBoardApp.kt
@@ -1,19 +1,21 @@
 package com.hyeeyoung.wishboard
 
 import android.app.Application
-import android.util.Log
 import com.amplifyframework.AmplifyException
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
 import com.amplifyframework.core.Amplify
 import com.amplifyframework.storage.s3.AWSS3StoragePlugin
 import com.hyeeyoung.wishboard.util.PreferenceUtil
+import com.hyeeyoung.wishboard.util.WishBoardDebugTree
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class WishBoardApp : Application() {
     override fun onCreate() {
         super.onCreate()
         prefs = PreferenceUtil(applicationContext)
+        setUpTimber()
 
         try {
             Amplify.addPlugin(AWSCognitoAuthPlugin())
@@ -22,6 +24,12 @@ class WishBoardApp : Application() {
             Log.d(TAG, "Initialized Amplify")
         } catch (error: AmplifyException) {
             Log.e(TAG, "Could not initialize Amplify", error)
+        }
+    }
+
+    private fun setUpTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(WishBoardDebugTree())
         }
     }
 
