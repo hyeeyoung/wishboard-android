0a3571f72beec234dc18286b0134c861f847e8f6
youngjinc <cyjin6789@gmail.com>
2022-10-25 08:09:06 +0900

    [refactor] 코드 정리

--- app/src/main/java/com/hyeeyoung/wishboard/data/repositories/WishRepositoryImpl.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/data/repositories/WishRepositoryImpl.kt
@@ -12,35 +12,26 @@ import javax.inject.Inject
 
 class WishRepositoryImpl @Inject constructor(private val wishItemService: WishItemService) :
     WishRepository {
-    override suspend fun fetchWishList(token: String): List<WishItem>? {
-        try {
-            val response = wishItemService.fetchWishList(token)
-            if (response.isSuccessful) {
-                Log.d(TAG, "아이템 가져오기 성공")
-            } else {
-                Log.e(TAG, "아이템 가져오기 실패: ${response.code()}")
-            }
-            return response.body()
-        } catch (e: Exception) {
-            Timber.e(e.message)
-            return null
-        }
-    }
+    override suspend fun fetchWishList(token: String): List<WishItem>? = runCatching {
+        wishItemService.fetchWishList(token)
+    }.fold({
+        Timber.d("아이템 가져오기 성공(${it.code()})")
+        it.body()
+    }, {
+        Timber.d("아이템 가져오기 실패: ${it.message}")
+        null
+    })
 
-    override suspend fun fetchLatestWishItem(token: String): WishItem? {
-        try {
-            val response = wishItemService.fetchLatestWishItem(token) ?: return null
-            if (response.isSuccessful) {
-                Log.d(TAG, "가장 최근 등록된 아이템 가져오기 성공")
-            } else {
-                Log.e(TAG, "가장 최근 등록된 아이템 가져오기 실패: ${response.code()}")
-            }
-            return response.body()?.get(0)
-        } catch (e: Exception) {
-            Timber.e(e.message)
-            return null
-        }
-    }
+    override suspend fun fetchLatestWishItem(token: String): WishItem? =
+        runCatching {
+            wishItemService.fetchLatestWishItem(token)
+        }.fold({
+            Timber.d("가장 최근 등록된 아이템 가져오기 성공(${it?.code()})")
+            it?.body()?.get(0)
+        }, {
+            Timber.e("가장 최근 등록된 아이템 가져오기 실패: ${it.message}")
+            null
+        })
 
     override suspend fun uploadWishItem(
         token: String,
@@ -52,72 +43,66 @@ class WishRepositoryImpl @Inject constructor(private val wishItemService: WishIt
         itemNotificationType: RequestBody,
         itemNotificationDate: RequestBody,
         image: MultipartBody.Part
-    ): Boolean {
-        try {
-            val response =
-                wishItemService.uploadWishItem(token, folderId, itemName, itemPrice, itemMemo, itemUrl, itemNotificationType, itemNotificationDate, image)
-            if (response.isSuccessful) {
-                Log.d(TAG, "아이템 등록 성공")
-            } else {
-                Log.e(TAG, "아이템 등록 실패: ${response.code()}")
-            }
-            return response.isSuccessful
-        } catch (e: Exception) {
-            Timber.e(e.message)
-            return false
-        }
-    }
+    ): Boolean = runCatching {
+        wishItemService.uploadWishItem(
+            token,
+            folderId,
+            itemName,
+            itemPrice,
+            itemMemo,
+            itemUrl,
+            itemNotificationType,
+            itemNotificationDate,
+            image
+        )
+    }.fold({
+        Timber.d("아이템 등록 성공(${it.code()})")
+        it.isSuccessful
+    }, {
+        Timber.e("아이템 등록 실패(${it.message})")
+        false
+    })
 
-    override suspend fun updateWishItem(token: String, itemId: Long, wishItem: WishItem): Pair<Boolean, Int>? {
-        return try {
-            val response = wishItemService.updateToWishItem(token, itemId, wishItem)
-            if (response.isSuccessful) {
-                Log.d(TAG, "아이템 수정 성공")
-            } else {
-                Log.e(TAG, "아이템 수정 실패: ${response.code()}")
-            }
-            Pair(response.isSuccessful, response.code())
-        } catch (e: Exception) {
-            e.printStackTrace()
-            null
-        }
-    }
+    override suspend fun updateWishItem(
+        token: String,
+        itemId: Long,
+        wishItem: WishItem
+    ): Pair<Boolean, Int>? = runCatching {
+        wishItemService.updateToWishItem(token, itemId, wishItem)
+    }.fold({
+        Timber.d("아이템 수정 성공(${it.code()})")
+        Pair(it.isSuccessful, it.code())
+    }, {
+        Timber.e("아이템 수정 실패: ${it.message}")
+        null
+    })
 
     override suspend fun updateFolderOfWishItem(
         token: String,
         itemId: Long,
         folderId: Long
-    ): Boolean {
-        try {
-            val response = wishItemService.updateFolderOfItem(token, itemId, folderId)
-            if (response.isSuccessful) {
-                Log.d(TAG, "아이템 폴더 수정 성공")
-            } else {
-                Log.e(TAG, "아이템 폴더 수정 실패: ${response.code()}")
-            }
-            return response.isSuccessful
-        } catch (e: Exception) {
-            e.printStackTrace()
-            return false
-        }
-    }
+    ): Boolean = runCatching {
+        wishItemService.updateFolderOfItem(token, itemId, folderId)
+    }.fold({
+        Timber.d("아이템 폴더 수정 성공(${it.code()})")
+        it.isSuccessful
+    }, {
+        Timber.e("아이템 폴더 수정 실패: ${it.message}")
+        false
+    })
 
-    override suspend fun deleteWishItem(token: String, itemId: Long): Boolean {
-        try {
-            val response = wishItemService.deleteWishItem(token, itemId)
-            if (response.isSuccessful) {
-                Log.d(TAG, "아이템 삭제 성공")
-            } else {
-                Log.e(TAG, "아이템 삭제 실패: ${response.code()}")
-            }
-            return response.isSuccessful
-        } catch (e: Exception) {
-            e.printStackTrace()
-            return false
-        }
-    }
+    override suspend fun deleteWishItem(token: String, itemId: Long): Boolean = runCatching {
+        wishItemService.deleteWishItem(token, itemId)
+    }.fold({
+        Timber.d("아이템 삭제 성공(${it.code()})")
+        it.isSuccessful
+    }, {
+        Timber.e("아이템 삭제 실패: ${it.message}")
+        false
+    })
 
-    override suspend fun getItemParsingInfo(site: String): ItemInfo? {
+    // TODO need refactoring
+    override suspend fun getItemParsingInfo(site: String): Pair<ItemInfo?, Int>? {
         return runCatching {
             wishItemService.getItemParsingInfo(site)
         }.fold({
