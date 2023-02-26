f0ea94351174a62884bfae6aca3ae4636bc46806
youngjinc <cyjin6789@gmail.com>
2023-02-26 11:42:50 +0900

    [add] NetworkModule 추가

---
+++ app/src/main/java/com/hyeeyoung/wishboard/domain/di/NetworkModule.kt
@@ -0,0 +1,56 @@
+package com.hyeeyoung.wishboard.domain.di
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.hyeeyoung.wishboard.BuildConfig
+import com.hyeeyoung.wishboard.BuildConfig.DEBUG
+import com.hyeeyoung.wishboard.data.interceptor.AuthInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideGson(): Gson = GsonBuilder().setLenient().create()
+
+    @Provides
+    @Singleton
+    fun provideAuthInterceptor(interceptor: AuthInterceptor): Interceptor = interceptor
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClientBuilder(
+        interceptor: AuthInterceptor,
+    ): OkHttpClient =
+        OkHttpClient.Builder().apply {
+            connectTimeout(10, TimeUnit.SECONDS)
+            writeTimeout(10, TimeUnit.SECONDS)
+            readTimeout(10, TimeUnit.SECONDS)
+            addInterceptor(interceptor)
+            if (DEBUG) addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            )
+        }.build()
+
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit = Retrofit.Builder()
+        .baseUrl(BuildConfig.BASE_URL)
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
