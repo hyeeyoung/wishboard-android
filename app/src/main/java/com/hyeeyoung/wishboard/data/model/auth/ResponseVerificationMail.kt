5287ff0937db604d37eddb2b862ef06d0393495a
youngjinc <cyjin6789@gmail.com>
2023-02-27 11:25:33 +0900

    [feat] 로그인 관련 api response 변경 사항 반영

---
+++ app/src/main/java/com/hyeeyoung/wishboard/data/model/auth/ResponseVerificationMail.kt
@@ -0,0 +1,5 @@
+package com.hyeeyoung.wishboard.data.model.auth
+
+data class ResponseVerificationMail(
+    val verificationCode: String
+)
--- app/src/main/java/com/hyeeyoung/wishboard/data/services/retrofit/AuthService.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/data/services/retrofit/AuthService.kt
@@ -1,10 +1,10 @@
 package com.hyeeyoung.wishboard.data.services.retrofit
 
 import com.hyeeyoung.wishboard.data.model.auth.RequestAuth
-import com.hyeeyoung.wishboard.data.model.auth.ResponseSignIn
-import com.hyeeyoung.wishboard.data.model.base.BaseResponseData
+import com.hyeeyoung.wishboard.data.model.auth.ResponseAuth
+import com.hyeeyoung.wishboard.data.model.auth.ResponseVerificationMail
+import com.hyeeyoung.wishboard.data.model.base.BaseResponse
 import com.hyeeyoung.wishboard.data.model.base.BaseResponseResult
-import com.hyeeyoung.wishboard.data.model.user.UserInfo
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.Field
@@ -14,30 +14,30 @@ import retrofit2.http.POST
 interface AuthService {
     @POST("auth/signup")
     suspend fun signUpUser(
-        @Body userInfo: UserInfo
-    ): Response<BaseResponseResult<BaseResponseData>>
+        @Body requestAuth: RequestAuth
+    ): Response<BaseResponseResult<ResponseAuth?>>
 
     @POST("auth/signin")
     suspend fun signInUser(
         @Body requestAuth: RequestAuth
-    ): Response<BaseResponseResult<ResponseSignIn?>>
+    ): Response<BaseResponseResult<ResponseAuth?>>
 
     @FormUrlEncoded
     @POST("auth/password-mail")
     suspend fun requestVerificationMail(
         @Field("email") email: String
-    ): Response<BaseResponseResult<BaseResponseData>>
+    ): Response<BaseResponseResult<ResponseVerificationMail?>>
 
     @FormUrlEncoded
     @POST("auth/re-signin")
     suspend fun signInEmail(
         @Field("verify") isVerify: Boolean,
         @Field("email") email: String
-    ): Response<BaseResponseResult<BaseResponseData>>
+    ): Response<BaseResponseResult<ResponseAuth?>>
 
     @FormUrlEncoded
     @POST("auth/check-email")
     suspend fun checkRegisteredUser(
         @Field("email") email: String
-    ): Response<BaseResponseResult<BaseResponseData?>>
+    ): Response<BaseResponse>
 }
