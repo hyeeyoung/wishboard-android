dda5a14e399a40ed984805e81deaeea4c5237c3b
youngjinc <cyjin6789@gmail.com>
2023-05-09 21:38:05 +0900

    [mod] 플로우 확장함수 타입 제한

--- app/src/main/java/com/hyeeyoung/wishboard/util/extension/FlowExt.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/util/extension/FlowExt.kt
@@ -2,6 +2,7 @@ package com.hyeeyoung.wishboard.util.extension
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.CoroutineScope
@@ -10,8 +11,8 @@ import kotlinx.coroutines.flow.*
 fun <T> Flow<T>.toStateFlow(coroutineScope: CoroutineScope, initialValue: T) =
     stateIn(coroutineScope, SharingStarted.Eagerly, initialValue)
 
-inline fun <T, R> R.collectFlow(
-    flow: StateFlow<T>, crossinline block: suspend (T) -> Unit
+inline fun <T, R: LifecycleOwner> R.collectFlow(
+    flow: Flow<T>, crossinline block: suspend (T) -> Unit
 ) {
     when (this) {
         is AppCompatActivity -> flow.flowWithLifecycle(lifecycle).onEach { block(it) }
