1b21dac9fcb607912377d2e3efb9e5ff01b530f1
youngjinc <cyjin6789@gmail.com>
2023-05-29 23:24:23 +0900

    [add] UiState > Init 타입 추가

--- app/src/main/java/com/hyeeyoung/wishboard/util/UiState.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/util/UiState.kt
@@ -1,6 +1,7 @@
 package com.hyeeyoung.wishboard.util
 
 sealed class UiState<out T> {
+    object Init : UiState<Nothing>()
     object Loading : UiState<Nothing>()
     object Empty : UiState<Nothing>()
     data class Success<T>(val data: T) : UiState<T>()
