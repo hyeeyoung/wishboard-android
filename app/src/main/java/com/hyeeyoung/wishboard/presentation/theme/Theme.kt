5863db19042c5a1939dd0a4c174d7fd66e0be667
youngjinc <cyjin6789@gmail.com>
2023-06-28 22:45:58 +0900

    [feat] 커스텀 typography 추가 및 적용

--- app/src/main/java/com/hyeeyoung/wishboard/presentation/theme/Theme.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/presentation/theme/Theme.kt
@@ -3,12 +3,9 @@ package com.hyeeyoung.wishboard.presentation.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
@@ -42,6 +39,7 @@ fun WishboardTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
+    typography: WishBoardTypography = WishBoardTheme.typography,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -60,9 +58,19 @@ fun WishboardTheme(
         }
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    CompositionLocalProvider(
+        LocalTypography provides typography
+    ) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            content = content
+        )
+    }
+}
+
+// Use with eg. ReplacementTheme.typography.body
+object WishBoardTheme {
+    val typography: WishBoardTypography
+        @Composable
+        get() = LocalTypography.current
 }
--- app/src/main/java/com/hyeeyoung/wishboard/presentation/theme/Type.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/presentation/theme/Type.kt
@@ -1,34 +1,131 @@
 package com.hyeeyoung.wishboard.presentation.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.hyeeyoung.wishboard.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+val SuitFamily = FontFamily(
+    Font(R.font.suit_b, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.suit_sb, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.suit_m, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.suit_r, FontWeight.Normal, FontStyle.Normal),
 )
+
+val MontserratFamily = FontFamily(
+    Font(R.font.montserrat_b, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.montserrat_sb, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.montserrat_m, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.montserrat_r, FontWeight.Normal, FontStyle.Normal),
+)
+
+@Immutable
+data class WishBoardTypography(
+    val suitH1: TextStyle = TextStyle(
+        fontFamily = SuitFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    val suitH2: TextStyle = TextStyle(
+        fontFamily = SuitFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+    ),
+    val suitH3: TextStyle = TextStyle(
+        fontFamily = SuitFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp
+    ),
+    val suitH4: TextStyle = TextStyle(
+        fontFamily = SuitFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    val suitH5: TextStyle = TextStyle(
+        fontFamily = SuitFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp
+    ),
+    val suitH6: TextStyle = TextStyle(
+        fontFamily = SuitFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 10.sp
+    ),
+    val suitB1: TextStyle = TextStyle(
+        fontFamily = SuitFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp
+    ),
+    val suitB2: TextStyle = TextStyle(
+        fontFamily = SuitFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp
+    ),
+    val suitB3: TextStyle = TextStyle(
+        fontFamily = SuitFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp
+    ),
+    val suitB4: TextStyle = TextStyle(
+        fontFamily = SuitFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 12.sp
+    ),
+    val suitB5: TextStyle = TextStyle(
+        fontFamily = SuitFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp
+    ),
+    val suitD1: TextStyle = TextStyle(
+        fontFamily = SuitFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    val suitD2: TextStyle = TextStyle(
+        fontFamily = SuitFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    val suitD3: TextStyle = TextStyle(
+        fontFamily = SuitFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    val montserratH1: TextStyle = TextStyle(
+        fontFamily = MontserratFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    val montserratH2: TextStyle = TextStyle(
+        fontFamily = MontserratFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+    ),
+    val montserratH3: TextStyle = TextStyle(
+        fontFamily = MontserratFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp
+    ),
+    val montserratB1: TextStyle = TextStyle(
+        fontFamily = MontserratFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp
+    ),
+    val montserratB2: TextStyle = TextStyle(
+        fontFamily = MontserratFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp
+    ),
+    val montserratD1: TextStyle = TextStyle(
+        fontFamily = MontserratFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 9.sp
+    )
+)
+
+val LocalTypography = staticCompositionLocalOf { WishBoardTypography() }
