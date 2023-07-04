a5f0aeeabf6b284e4409323debda757be394d972
youngjinc <cyjin6789@gmail.com>
2023-07-04 11:38:41 +0900

    [mod] 상태바 컬러 변경

--- app/src/main/java/com/hyeeyoung/wishboard/designsystem/theme/Theme.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/designsystem/theme/Theme.kt
@@ -1,16 +1,11 @@
 package com.hyeeyoung.wishboard.designsystem.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -37,32 +32,13 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun WishboardTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     typography: WishBoardTypography = WishBoardTheme.typography,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
-        }
-    }
-
     CompositionLocalProvider(
         LocalTypography provides typography
     ) {
         MaterialTheme(
-            colorScheme = colorScheme,
             content = content
         )
     }
